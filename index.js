5e42f522128d33312b406de97b6009b05a7b5032
gor-dey <gordeychepikov@gmail.com>
2022-12-28 19:24:00 +0600

    mobile-main-page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// SWIPER MOBILE START
+// SWIPER MOBILE WINNERS START
 new Swiper(".swiper", {
   slidesPerView: 2.4,
   spaceBetween: 5,
 });
-// SWIPER MOBILE END
+// SWIPER MOBILE WINNERS END
+
+// SWIPER MOBILE TICKET START
+new Swiper(".swiper--ticket", {
+  slidesPerView: 1.5,
+  spaceBetween: 5,
+});
+// SWIPER MOBILE TICKET END
